05fe370ab4eface28d042420d410ee39903f76b5
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2020-03-28 16:56:46 +0200

    UI redesign

--- ChitankaMobileUI/ChitankaMobileUI.Android/Resources/Resource.designer.cs
+++ ChitankaMobileUI/ChitankaMobileUI.Android/Resources/Resource.designer.cs
@@ -5794,22 +5794,25 @@ namespace ChitankaMobileUI.Droid
 			public const int notify_panel_notification_icon_bg = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int settings_icon = 2131165302;
+			public const int refresh_icon = 2131165302;
 			
 			// aapt resource value: 0x7F070077
-			public const int tab_about = 2131165303;
+			public const int settings_icon = 2131165303;
 			
 			// aapt resource value: 0x7F070078
-			public const int tab_feed = 2131165304;
+			public const int tab_about = 2131165304;
 			
 			// aapt resource value: 0x7F070079
-			public const int tooltip_frame_dark = 2131165305;
+			public const int tab_feed = 2131165305;
 			
 			// aapt resource value: 0x7F07007A
-			public const int tooltip_frame_light = 2131165306;
+			public const int tooltip_frame_dark = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int xamarin_logo = 2131165307;
+			public const int tooltip_frame_light = 2131165307;
+			
+			// aapt resource value: 0x7F07007C
+			public const int xamarin_logo = 2131165308;
 			
 			static Drawable()
 			{
--- ChitankaMobileUI/ChitankaMobileUI/Views/BookDetailView.xaml.cs
+++ ChitankaMobileUI/ChitankaMobileUI/Views/BookDetailView.xaml.cs
@@ -13,5 +13,22 @@ namespace ChitankaMobileUI.Views
             InitializeComponent();
             BindingContext = new BookDetailViewModel(book);
         }
+
+        private void DescriptionScroll_Scrolled(object sender, ScrolledEventArgs e)
+        {
+            ScrollView scrollView = sender as ScrollView;
+            double scrollingSpace = scrollView.ContentSize.Height - scrollView.Height;
+            double errorPercent = 0.8;
+
+            // Touched bottom
+            if (scrollingSpace * errorPercent <= e.ScrollY)
+            {
+                DescriptionGradient.FadeTo(0, 250, Easing.Linear);
+            }
+            else
+            {
+                DescriptionGradient.FadeTo(1, 250, Easing.Linear);
+            }
+        }
     }
 }
